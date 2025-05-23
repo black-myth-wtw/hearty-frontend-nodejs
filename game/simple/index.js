81e583e3f70d7e9cab761347cc197c1729b3b1a9
w-t-w <15866369958@163.com>
2025-05-24 07:24:20 +0800

    NodeJS《第二课: 简易的模拟石头、剪刀与布游戏大赛》部分课程已完美的回溯完成~

---
+++ game/simple/index.js
@@ -0,0 +1,32 @@
+// const player_action_params = process.argv,
+//     player_action_params_length = player_action_params.length;
+//
+// const player_action = player_action_params[player_action_params_length - 1];
+//
+// const computer_selections = ['scissor', 'rock', 'paper'];
+//
+// if (!computer_selections.includes(player_action)) {
+//     console.error('您输入的模拟石头、剪刀与布游戏大赛手势不符合规范!请重新输入!【scissor rock or paper】');
+//     process.exit(1);
+// }
+//
+// const computer_selections_length = computer_selections.length;
+//
+// const computer_random = Math.floor(Math.random() * computer_selections_length);
+//
+// const computer_action = computer_selections[computer_random];
+//
+// let player_win_index = computer_random + 1;
+// if (player_win_index >= computer_selections_length) player_win_index = 0;
+//
+// console.log('第一届模拟石头、剪刀与布游戏大赛正式开始!');
+// console.log(`玩家: ${player_action}, 电脑: ${computer_action}`);
+// if (player_action === computer_action) {
+//     console.log('平局!');
+// } else if (player_action === computer_selections[player_win_index]) {
+//     console.log('玩家赢!');
+// } else {
+//     console.log('电脑赢!');
+// }
+
+console.log('NodeJS《第二课: 简易的模拟石头、剪刀与布游戏大赛》部分课程已完美的回溯完成!如需要检测执行结果,将以上代码注释解开即可!');
